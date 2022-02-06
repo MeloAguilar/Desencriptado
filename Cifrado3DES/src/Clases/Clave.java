1f56d5561f20a864f907553cac9056549fc52f76
origin <https://github.com/MeloAguilar/Desencriptado.git>
2022-02-06 18:42:30 +0100

    Claves terminadas. Falta encriptacion

--- Cifrado3DES/src/Clases/Clave.java
+++ Cifrado3DES/src/Clases/Clave.java
@@ -1,27 +1,94 @@
 package Clases;
 
 import javax.crypto.*;
+import javax.crypto.spec.DESKeySpec;
 import javax.crypto.spec.DESedeKeySpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.security.InvalidKeyException;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
 
 public class Clave {
-    private static final String ALGORITMO_CLAVE_SIMETRICA = "DESede";  // 3DES
-    private static final String NOM_FICH_CLAVE = "C:\\Users\\caguilar.INFO2\\Desktop\\PSPRO\\programas_ejemplo_libro_PSP\\Tema5-TecnicasProgSegura\\03-Cifrado3DES\\Cifrado3DES\\src\\Claves\\";
+    private static final String ALGORITMO_CLAVE_SIMETRICA_DESede = "DESede";// 3DES
+    private static final String ALGORITMO_CLAVE_SIMETRICA_DES = "DES";
+    private static final String ALGORITMO_CLAVE_SIMETRICA_AES = "AES";
+    //C:\Users\caguilar.INFO2\Desktop\PSPRO\programas_ejemplo_libro_PSP\Tema5-TecnicasProgSegura\03-Cifrado3DES\Cifrado3DES\src\Claves\
+    private static final String NOM_FICH_CLAVE = "C:\\Users\\GL512\\IdeaProjects\\Desencriptado\\Cifrado3DES\\src\\Claves\\";
     private static final String ALGORITMO_GEN_NUM_ALEAT = "SHA1PRNG";
 
 
-    public static String generarClave(String ficheroClave, String rutaCompletaFicheroAEncriptar, String ficheroEncriptadoACrear) {
+    /**
+     * Método que se encarga de generar una clave AES
+     * y devuelve la ruta del archivo donde se encuentra esta
+     *
+     * @param ficheroClave
+     * @return
+     */
+    public static String generarClaveAES(String ficheroClave) {
+        try {
+            //Creo la instancia del generador de claves
+            KeyGenerator genClaves = KeyGenerator.getInstance(ALGORITMO_CLAVE_SIMETRICA_AES);
+            //Creo la instancia del random
+            SecureRandom srand = SecureRandom.getInstance(ALGORITMO_GEN_NUM_ALEAT);
+            //inicializo el generador de claves con el número random creado anteriormente
+            genClaves.init(srand);
+            //Inistancio una clave secreta que sera generada por genClaves
+            //a partir del número aleatorio dado anteriormente
+            SecretKey clave = genClaves.generateKey();
+            //Muestro el formato del fichero que contendra la clave
+            System.out.printf("Formato de clave: %s\n", clave.getFormat());
+            //Convierto esta clave en un array de bytes y lo guardo
+            byte[] claveEncriptacion = clave.toString().getBytes("UTF-8");
 
-        try{
-            KeyGenerator genClaves = KeyGenerator.getInstance(ALGORITMO_CLAVE_SIMETRICA);
+
+            MessageDigest sha = MessageDigest.getInstance("SHA-1");
+            claveEncriptacion = sha.digest(claveEncriptacion);
+            claveEncriptacion = Arrays.copyOf(claveEncriptacion, 16);
+            SecretKeySpec secretKey = new SecretKeySpec(claveEncriptacion, ALGORITMO_CLAVE_SIMETRICA_AES);
+            byte[] valorClave = secretKey.getEncoded();
+            try (FileOutputStream fos = new FileOutputStream(NOM_FICH_CLAVE + ficheroClave + ".txt")) {
+                fos.write(valorClave);
+            } catch (IOException e) {
+                System.out.println("Error de E/S escribiendo clave en fichero");
+                e.printStackTrace();
+            }
+            System.out.printf("Longitud de clave: %d bits\n", valorClave.length * 8);
+            System.out.printf("Valor de la clave: [%s] \nen fichero %s\n",
+                    valorHexadecimal(valorClave), NOM_FICH_CLAVE + ficheroClave);
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("No se encuentra el algoritmo");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return NOM_FICH_CLAVE + ficheroClave;
+    }
+
+
+    /**
+     * Método que genera una clave DESede.
+     * Devuelve la ruta completa del fichero creado con la clave DESede
+     *
+     * @param ficheroClave
+     * @return
+     */
+    public static String generarClaveDESyDESede(int tipoEncriptado, String ficheroClave) {
+        String algoritmo = "";
+        switch (tipoEncriptado) {
+            case 2:
+                algoritmo = ALGORITMO_CLAVE_SIMETRICA_DES;
+                break;
+            case 3:
+                algoritmo = ALGORITMO_CLAVE_SIMETRICA_DESede;
+        }
+
+        try {
+            KeyGenerator genClaves = KeyGenerator.getInstance(algoritmo);
 
             SecureRandom srand = SecureRandom.getInstance(ALGORITMO_GEN_NUM_ALEAT);
             genClaves.init(srand);
@@ -30,30 +97,34 @@ public class Clave {
             SecretKey clave = genClaves.generateKey();
             System.out.printf("Formato de clave: %s\n", clave.getFormat());
 
-            SecretKeyFactory keySpecFactory = SecretKeyFactory.getInstance(ALGORITMO_CLAVE_SIMETRICA);
-
-            DESedeKeySpec keySpec = (DESedeKeySpec) keySpecFactory.getKeySpec(clave, DESedeKeySpec.class);
-
-            byte[] valorClave = keySpec.getKey();
-
-            System.out.printf("Longitud de clave: %d bits\n", valorClave.length*8);
+            SecretKeyFactory keySpecFactory = SecretKeyFactory.getInstance(algoritmo);
+            KeySpec keySpec = null;
+            byte[] valorClave = null;
+            if (algoritmo.equals(ALGORITMO_CLAVE_SIMETRICA_DESede)) {
+                keySpec = keySpecFactory.getKeySpec(clave, DESedeKeySpec.class);
+                valorClave = ((DESedeKeySpec) keySpec).getKey();
+            } else if (algoritmo.equals(ALGORITMO_CLAVE_SIMETRICA_DES)) {
+                keySpec = keySpecFactory.getKeySpec(clave, DESKeySpec.class);
+                valorClave = ((DESKeySpec) keySpec).getKey();
+            }
+            System.out.printf("Longitud de clave: %d bits\n", valorClave.length * 8);
             System.out.printf("Valor de la clave: [%s] en fichero %s",
-                    valorHexadecimal(valorClave), NOM_FICH_CLAVE+ficheroClave);
-            try(FileOutputStream fos = new FileOutputStream(NOM_FICH_CLAVE+ficheroClave+".txt")) {
+                    valorHexadecimal(valorClave), NOM_FICH_CLAVE + ficheroClave);
+            try (FileOutputStream fos = new FileOutputStream(NOM_FICH_CLAVE + ficheroClave + ".txt")) {
                 fos.write(valorClave);
             } catch (IOException e) {
                 System.out.println("Error de E/S escribiendo clave en fichero");
                 e.printStackTrace();
             }
 
+
         } catch (NoSuchAlgorithmException e) {
             System.out.println("Algoritmo de generación de claves no soportado");
         } catch (InvalidKeySpecException e) {
             e.printStackTrace();
         }
-        return NOM_FICH_CLAVE+ficheroClave;
-        }
-
+        return NOM_FICH_CLAVE + ficheroClave;
+    }
 
 
     static String valorHexadecimal(byte[] bytes) {
--- Cifrado3DES/src/Main.java
+++ Cifrado3DES/src/Main.java
@@ -3,40 +3,105 @@ import Clases.*;
 import java.util.Scanner;
 
 public class Main {
+    public static final String MENSAJE_ERROR = "La elección debe ser un número entre 0 y 3";
+    public static final String PEDIR_NOMARCHIVO = "Escribe el nombre del fichero donde quieres que se genere la clave";
+    public static final String PEDIR_TIPO_ENCRIPTADO = "Elija entre los tres tipos de encriptado posibles. \n" +
+            "1 -> AES \n" +
+            "2 -> DES \n" +
+            "3 -> DESede";
+    public static final String PEDIR_TIPO_CLAVE = "Escribe: \n" +
+            "1 -> AES \n" +
+            "2 -> DES \n" +
+            "3 -> DESede\n" +
+            "0 -> Atrás";
+    public static final String RUTA_CLAVES = "C:\\Users\\GL512\\IdeaProjects\\Desencriptado\\Cifrado3DES\\src\\Claves\\";
 
     public static void main(String[] args) {
-        Scanner sc =  new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         menu(sc);
     }
 
-private static String pedirFicheroPaLaClave(Scanner sc){
-    System.out.println("Escribe el nombre del fichero donde quieres que se genere la clave");
+
+    /**
+     * @param sc
+     * @param peticion
+     * @return
+     */
+    private static String pedir(Scanner sc, String peticion) {
+        System.out.println(peticion);
         return sc.nextLine();
-}
+    }
 
-private static void opcion1(Scanner sc){
-        Clave.generarClave(pedirFicheroPaLaClave(sc));
-}
+    /**
+     * @param sc
+     * @return
+     */
+    private static int eleccionTipoClave(Scanner sc) {
+        String eleccionString = "";
+        int eleccion = -1;
+        do {
+            eleccionString = pedir(sc, PEDIR_TIPO_CLAVE);
+            try {
+                eleccion = Integer.parseInt(eleccionString);
+            } catch (Exception e) {
+                System.out.println(MENSAJE_ERROR);
+                eleccion = -1;
+            }
+        } while (eleccion == -1);
+        return eleccion;
+    }
 
-private static void menu(Scanner sc){
+
+    /**
+     * Desde aquí se llama al método que pedirá al usuario
+     * introducir el tipo de clave de cifrado que prefiere
+     * para su fichero.
+     *
+     * @param sc
+     */
+    private static void opcion1(Scanner sc, int tipoClave) {
+        switch (tipoClave) {
+            case 1:
+                Clave.generarClaveAES(pedir(sc, PEDIR_NOMARCHIVO));
+                break;
+            case 2:
+                Clave.generarClaveDESyDESede(eleccionTipoClave(sc), pedir(sc, PEDIR_NOMARCHIVO));
+                break;
+        }
 
 
-    int eleccion = 0;
-        do{
+    }
+
+
+    /**
+     * Menu de la app
+     *
+     * @param sc
+     */
+    private static void menu(Scanner sc) {
+
+
+        int eleccion = 0;
+        do {
             System.out.println("Elige una opcion \n" +
                     "1 -> Generar Clave \n" +
                     "0 -> Salir");
             String input = sc.nextLine();
             try {
                 eleccion = Integer.parseInt(input);
-            }catch(NumberFormatException ex){
+            } catch (NumberFormatException ex) {
                 System.out.println("No es una opcion válida");
                 eleccion = -1;
             }
-            switch(eleccion){
-                case 1: opcion1(sc);
+            switch (eleccion) {
+                case 0:
+                    System.out.println("Bye!");
+                    break;
+                case 1:
+                    opcion1(sc, eleccionTipoClave(sc));
+                    break;
             }
-        }while(eleccion != 0);
+        } while (eleccion != 0);
 
-}
+    }
 }
